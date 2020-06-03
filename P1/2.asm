1afbd02aada6eedba89fb26f74eeafcf48157d9b
David <davidgilbautista@gmail.com>
2020-06-03 13:03:09 +0200

    2 acabado

--- P1/2.asm
+++ P1/2.asm
@@ -1,106 +1,131 @@
-punto MACRO X, Y, C
-    push ax
-    push cx
-    push dx
+TEXTO40x25BN	EQU 0
+TEXTO40x25C		EQU 1
+TEXTO80x25BN	EQU 2
+TEXTO80x25C		EQU 3
+GRAFICO EQU 4
 
-    mov ax, Y
-    mov cx, X
-    mov dx, ax
-    mov al, C
-    mov ah, 0Ch
-    int 10h
+CYAN EQU 1
+ROSA  EQU 2
+BLANCO EQU 3
 
-    pop dx
-    pop cx
-    pop ax
+; MACRO pausa
+; Espera la pulsacion de una tecla
+pausa MACRO
+        push ax
+        mov ah,0   ;funcion para leer una tecla
+        int 16h    ;interrupcion BIOS para teclado
+        pop ax
 ENDM
 
-
-
-rectangulo MACRO x, y, f, c
-
-    mov cx, 0
-    mov dx, x
-	bucle:
-    	pixel dx, y, 3
-    	inc dx
-	    inc cx
-    	cmp cx, f
-	    jne bucle
-
-    mov cx, 0
-    mov ax, y
-	bucle1:
-    	pixel dx, ax, 3
-	    inc ax
-    	inc cx
-	    cmp cx, c
-    	jne bucle1
-
-    mov cx, 0
-	bucle2:
-    	pixel dx, ax, 3
-	    dec dx
-	    inc cx
-	    cmp cx, f
-	    jne bucle2
-
-    mov cx, 0
-	bucle3:
-	    pixel dx, ax, 3
-	    dec ax
-	    inc cx
-	    cmp cx, c
-	    jne bucle3
-
-	;; Añadir puntos al rectángulo
-    add dx, 4
-    add ax, 3
-    pixel dx, ax, 1
-
-    add dx, 4
-    add ax, 3
-    pixel dx, ax, 2
+; MACRO modo_video
+; Cambia el modo de video
+modo_video MACRO modo
+        push ax
+        mov al,modo
+        mov ah,0
+        int 10h
+        pop ax
 ENDM
 
-    pila segment stack 'stack'
-        dw 100h dup (?)
-    pila ends
+; MACRO pixel
+; Pone un pixel en la coordenada X,Y de color C
+pixel MACRO X,Y,C
+        push ax
+        push cx
+		push dx
+		mov ax,Y
+		mov cx,X
+		mov dx,ax
+        mov al,C
+        mov ah,0Ch
+        int 10h
+        pop dx
+        pop cx
+		pop ax
+ENDM
 
-    datos segment 'data'
-        Cadena db 13,10,'pulsa una tecla...',13,10,'$'
-    datos ends
+pila segment stack 'stack'
+	dw 110h dup (?)
+pila ends
 
-    codigo segment 'code'
-        assume cs:codigo, ds:datos, ss:pila
-        main PROC
-            mov ax, datos
-            mov ds, ax
-
-            ;; Modo gráfico 320x200
-            mov al, 4
-            mov ah, 0
-            int 10h
-
-
-            ;; Pintar pixeles
-            rectangulo 50, 50, 100, 50
-
-
-            ;; Esperar la pulsación de una tecla
-            mov ah,8
-            int 21h
-
-            ;; Modo 80x25
-            mov al, 3
-            mov ah, 0
-            int 10h
+datos segment 'data'
+		msg_1 db 13,10,'Presione cualquier tecla para entrar en modo grafico...',13,10,'$'
+		msg_2 db 13,10,'Modo texto 80x25 restaurado.',13,10,'$'
+datos ends
 
 
-            mov ax, 4C00h
-            int 21h
+codigo segment 'code'
+	assume cs:codigo, ds:datos, ss:pila
+	main PROC
+		mov ax,datos
+		mov ds,ax
 
-        main ENDP
+		mov dx,OFFSET msg_1
+		mov ah,9
+		int 21h
+		pausa
 
-    codigo ends
-    END main
+        ; Pasamos a modo gráfico
+		modo_video GRAFICO
+
+		; linea horizontal superior
+		mov cx,240
+        mov bx,80
+		bucle1:
+			pixel cx,90,BLANCO
+			dec cx
+            cmp cx,bx
+			jne bucle1
+
+        ; linea horizontal inferior
+		mov cx,240
+        mov bx,79
+		bucle2:
+			pixel cx,110,BLANCO
+			dec cx
+            cmp cx,bx
+			jne bucle2
+
+        ; linea vertical izquierda
+		mov cx,90
+        mov bx,110
+		bucle3:
+			pixel 80,cx,BLANCO
+			inc cx
+            cmp cx,bx
+			jne bucle3
+
+        ; linea vertical derecha
+		mov cx,90
+        mov bx,110
+		bucle4:
+			pixel 240,cx,BLANCO
+			inc cx
+            cmp cx,bx
+			jne bucle4
+
+        ; Punto ROSA
+		pixel 90,100,ROSA
+
+    ; Punto CYAN
+    pixel 160,100,CYAN
+
+    ; Punto BLANCO
+    pixel 230,100,BLANCO
+
+
+		; Pausamos y volvemos a 80x25
+		pausa
+		modo_video TEXTO80x25C
+
+    mov dx,OFFSET msg_2
+		mov ah,9
+		int 21h
+
+		mov ax,4C00h
+		int 21h
+
+	main ENDP
+codigo ends
+
+END main
--- P1/readme.md
+++ P1/readme.md
@@ -8,3 +8,12 @@
 ![](imgs/2.png)
 
 ![](imgs/3.png)
+
+
+2. Programa que use la función de interrupción de cambio de modo de vídeo (usar el modo gráfico 320x200 color) y dibujar un rectángulo blanco en pantalla; a continuación, mostrar varios pixels de diferentes colores dentro del rectángulo.
+
+![](imgs/4.png)
+
+![](imgs/5.png)
+
+![](imgs/6.png)
